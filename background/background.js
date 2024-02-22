5dae4c08db3e5db30bb44893aff06bf3433c42b0
~ <37386853+tilde-nya@users.noreply.github.com>
2024-02-22 11:43:53 +0000

    move default settings to background.js

--- background/background.js
+++ background/background.js
@@ -1,3 +1,24 @@
+chrome.runtime.onInstalled.addListener(function (details) {
+    if (details.reason == "install") {
+        chrome.storage.local.set({
+            apiKey: "",
+            minimize_crm: {
+                log_communication: false,
+                activity_log: false,
+                outlet_summary: true,
+                contacts: true,
+                upcoming_tasks_and_sales_visits: true,
+                recently_ordered_products: true,
+                recent_orders: false,
+                recent_credit_notes: true,
+                containers_at_outlet: true
+            },
+            discount_warning: true,
+            discount_warning_text: "<h2>SET DISCOUNT BASIS TO <b>£ PER B. BARREL</b>, NOT %!!</h2>"
+        });
+    }
+});
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status == "complete") {
         chrome.tabs.sendMessage(tabId, {
